6cd1deab8725871e83a17909e3f61be798768661
Walter Ehrenberger <wehrenberger@salesforce.com>
2022-10-25 17:00:44 +0200

    Coding warmup lecture 1

--- exam_assignments/1_lecture/coding_warmup/slide13_hello.cpp
+++ exam_assignments/1_lecture/coding_warmup/slide13_hello.cpp
@@ -7,7 +7,6 @@ int main()
 {
 #pragma omp parallel // compiler directive
     {
-        // omp_get_thread_num() is an OpenMP library routine
         int thread_id = omp_get_thread_num();
         cout << "Hello from thread " << thread_id << endl;
     }
