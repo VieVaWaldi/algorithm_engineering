64ca2f5f5ebd7fd40817deb7a399ec3372e23c6b
Walter Ehrenberger <wehrenberger@salesforce.com>
2022-11-15 18:45:17 +0100

    ass 3

--- exam_assignments/3_lecture/warmup/parallel_pi_one_line.cpp
+++ exam_assignments/3_lecture/warmup/parallel_pi_one_line.cpp
@@ -50,12 +50,10 @@ void integral_pi_parallel()
     double sum = 0.0;                       // for summing up all heights of rectangles
 
 #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < num_steps; i++)
     {
-        for (int i = 0; i < num_steps; i++)
-        {
-            double x = (i + 0.5) * width;      // midpoint
-            sum = sum + (1.0 / (1.0 + x * x)); // add new height of a rectangle
-        }
+        double x = (i + 0.5) * width;      // midpoint
+        sum = sum + (1.0 / (1.0 + x * x)); // add new height of a rectangle
     }
     double pi = sum * 4 * width; // compute pi
     cout << "pi with " << num_steps << " steps is " << setprecision(17)
